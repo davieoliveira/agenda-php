6ec5531c2ea490ea8aaccba76b3529e300d70f0c
davieoliveira <emanuel.davi@escolar.ifrn.edu.br>
2026-07-24 02:05:32 -0300

    Definida estrutura base do projeto e index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Agendados</title>
+</head>
+<body>
+    <header> 
+        <h1>Sistema Agendador</h1>
+        <nav>
+            <a href="index.php?menuop=home"> Home </a> |
+            <a href="index.php?menuop=contatos "> Contatos </a> |
+            <a href="index.php?menuop=tarefas"> Tarefas </a> |
+            <a href="index.php?menuop=eventos"> Eventos </a> 
+        </nav>
+    </header>
+    <main>
+        <?php
+        $menuop = (isset($_GET["menuop"]))?$_GET["menuop"]:"home";
+        switch($menuop){
+            case 'home':
+                include("paginas/home/home.php");
+                break;
+            case 'contatos':
+                include("paginas/contatos/contatos.php");
+                break;
+            case 'tarefas':
+                include("paginas/tarefas/tarefas.php");
+                break;
+            default:
+                include("paginas/eventos/eventos.php");
+                #code...
+                break;
+            }
+        ?>
+    </main>
+</body>
+</html>
