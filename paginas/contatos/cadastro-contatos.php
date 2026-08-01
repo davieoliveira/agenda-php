c26098101990599e965173a3c1049b9e5a07c3ad
davieoliveira <emanuel.davi@escolar.ifrn.edu.br>
2026-08-01 19:53:46 -0300

    Criado form de cadastro de contato

---
+++ paginas/contatos/cadastro-contatos.php
@@ -0,0 +1,28 @@
+<header>
+    <h3>Cadastro de Contato</h3>
+</header>
+<div>
+    <form action="" method="post">
+        <div>
+            <label for="nomeContato"> Nome</label>
+            <input type="text" name="nomeContato">
+        </div>
+        <div>
+            <label for="emailContato"> Nome</label>
+            <input type="email" name="emailContato">
+        </div>
+        <div>
+            <label for="telefoneContato"> Nome</label>
+            <input type="text" name="telefoneContato">
+        </div>
+        <div>
+            <label for="dataNascContato"> Nome</label>
+            <input type="text" name="dataNascContato">
+        </div>
+        <div>
+            <label for="sexoContato"> Nome</label>
+            <input type="text" name="sexoContato">
+        </div>
+        <input type="submit" value="Adicionar" name="btnAdicionar"
+    </form>
+</div>
--- paginas/contatos/contatos.php
+++ paginas/contatos/contatos.php
@@ -1,33 +1,36 @@
 <header>
     <h3>Pagina de contatos</h3>
-    <table border="1">
-        <thead>
-            <tr>
-                <th> ID </th>
-                <th> Nome </th>
-                <th> Email </th>
-                <th> Telefone </th>
-                <th> Sexo </th>
-                <th> Data Nascimento </th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php
-        $sql = "SELECT * FROM tbcontatos";
-        $rs = mysqli_query($conexao, $sql) or die("Erro ao executar consulta!" . mysqli_error($conexao));
-        while($dados = mysqli_fetch_assoc($rs)){
-        ?>        
-            <tr>
-                <td> <?=$dados["id"] ?>               </td>
-                <td> <?=$dados["nomeContato"] ?>      </td>
-                <td> <?=$dados["emailContato"] ?>     </td>
-                <td> <?=$dados["telefoneContato"] ?>  </td>
-                <td> <?=$dados["sexoContato"] ?>      </td>
-                <td> <?=$dados["dataNascData"] ?>     </td>
-            </tr>
-        <?php
-        }
-        ?>
-        </tbody>
-    </table>
 </header>
+<div>
+    <a href="index.php?menuop=cadastro-contato">Novo Contato</a>
+</div>
+<table border="1">
+    <thead>
+        <tr>
+            <th> ID </th>
+            <th> Nome </th>
+            <th> Email </th>
+            <th> Telefone </th>
+            <th> Sexo </th>
+            <th> Data Nascimento </th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php
+    $sql = "SELECT * FROM tbcontatos";
+    $rs = mysqli_query($conexao, $sql) or die("Erro ao executar consulta!" . mysqli_error($conexao));
+    while($dados = mysqli_fetch_assoc($rs)){
+    ?>        
+        <tr>
+            <td> <?=$dados["id"] ?>               </td>
+            <td> <?=$dados["nomeContato"] ?>      </td>
+            <td> <?=$dados["emailContato"] ?>     </td>
+            <td> <?=$dados["telefoneContato"] ?>  </td>
+            <td> <?=$dados["sexoContato"] ?>      </td>
+            <td> <?=$dados["dataNascData"] ?>     </td>
+        </tr>
+    <?php
+    }
+    ?>
+    </tbody>
+</table>
