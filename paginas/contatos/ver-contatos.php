4c68b83de58fda2fb6017915e918dc55228f96c5
davieoliveira <emanuel.davi@escolar.ifrn.edu.br>
2026-08-02 20:41:02 -0300

    Adicionada paginação da agenda de contatos

--- paginas/contatos/ver-contatos.php
+++ paginas/contatos/ver-contatos.php
@@ -26,6 +26,12 @@
     </thead>
     <tbody>
     <?php
+
+    $quantidade = 10;
+    $pagina = (isset($_GET['pagina']))?(int)$_GET['pagina']:1;
+
+    $inicio = ($quantidade * $pagina) - $quantidade;
+
     $txt_pesquisa = (isset($_POST["txt_pesquisa"]))?$_POST["txt_pesquisa"]:"";
 
     $sql = "SELECT  
@@ -44,6 +50,7 @@
             FROM tbcontatos WHERE idContato='{$txt_pesquisa}' or nomeContato
             LIKE '%{$txt_pesquisa}%'
             ORDER BY nomeContato ASC
+            LIMIT $inicio, $quantidade
             ";
 
     $rs = mysqli_query($conexao, $sql) or die("Erro ao executar consulta!" . mysqli_error($conexao));
@@ -65,3 +72,35 @@
     ?>
     </tbody>
 </table>
+<br>
+<?php
+$sqlTotal = "SELECT idContato FROM tbContatos";
+$qrTotal = mysqli_query($conexao, $sqlTotal) or die(mysqli_error($conexao));
+$numTotal = mysqli_num_rows($qrTotal);
+$totalPagina = ceil($numTotal/$quantidade);
+echo "Total de Contatos: $numTotal <br>";
+echo "<a href=\"?menuop=contatos&pagina=1\">Primeira Página</a>";
+
+if($pagina>6){
+    ?>
+    <a href="?menuop=contatos&pagina=<?php echo $pagina-1?>"> << </a>
+    <?php
+}
+
+for($i=1; $i<=$totalPagina; $i++){
+    if($i >= ($pagina-5) && $i <= ($pagina + 5)){
+        if($i == $pagina){
+            echo $i;
+        }else{
+            echo "<a href=\"?menuop=contatos&pagina=$i\">$i</a> ";
+        }
+    }
+}
+if($pagina< ($totalPagina-5)){
+    ?>
+    <a href="?menuop=contatos&pagina=<?php echo $pagina+1?>"> >> </a>
+    <?php
+}
+
+echo "<a href=\"?menuop=contatos&pagina=$totalPagina\">Ultima Página</a>";
+?>
