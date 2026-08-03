3b4e8c2381aa6b6ca5a7c4f0526c587f235881e4
davieoliveira <emanuel.davi@escolar.ifrn.edu.br>
2026-08-03 12:47:52 -0300

    Adicionado bootstrap na paginação tabelas e navbar

--- index.php
+++ index.php
@@ -5,22 +5,45 @@ include("db/conexao.php");
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema Agendados</title>
 </head>
 <body>
     <header> 
-        <h1>Sistema Agendador</h1>
-        <nav>
-            <a href="index.php?menuop=home"> Home </a> |
-            <a href="index.php?menuop=contatos "> Contatos </a> |
-            <a href="index.php?menuop=tarefas"> Tarefas </a> |
-            <a href="index.php?menuop=eventos"> Eventos </a> 
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Projeto Agenda</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php?menuop=home">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="index.php?menuop=contatos ">Contatos</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="index.php?menuop=tarefas">Tarefas</a>
+                </li>
+                
+                <li class="nav-item">
+                <a class="nav-link" href="index.php?menuop=eventos">Eventos</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            </div>
+        </div>
         </nav>
     </header>
     <main>
-        <hr>
         <?php
         $menuop = (isset($_GET["menuop"]))?$_GET["menuop"]:"home";
         switch($menuop){
@@ -55,5 +78,7 @@ include("db/conexao.php");
             }
         ?>
     </main>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
 </body>
 </html>
--- paginas/contatos/ver-contatos.php
+++ paginas/contatos/ver-contatos.php
@@ -1,16 +1,17 @@
-<header>
-    <h3>Pagina de contatos</h3>
-</header>
-<div>
-    <a href="index.php?menuop=cadastro-contato">Novo Contato</a>
-</div>
-<div>
-    <form action="index.php?menuop=contatos" method="post"> 
-        <input type="text" name="txt_pesquisa">
-        <input type="submit" value="Pesquisar"> 
+<div class="d-flex align-items-center justify-content-center position-relative my-4">
+    <form class="input-group" style="max-width: 350px;" action="index.php?menuop=contatos" method="post"> 
+        <input class="form-control me-2" type="text" name="txt_pesquisa" placeholder="Buscar contato...">
+        <input class="btn btn-outline-secondary" type="submit" value="Pesquisar"> 
     </form>
+
+    <div class="position-absolute end-0">
+        <a class="btn btn-success" href="index.php?menuop=cadastro-contato">
+            <i class="bi bi-plus-lg"></i> Novo Contato
+        </a>
+    </div>
 </div>
-<table border="1">
+
+<table  class="table table-dark table-striped">
     <thead>
         <tr>
             <th> ID </th>
@@ -64,8 +65,8 @@
             <td> <?=$dados["enderecoContato"] ?>  </td>
             <td> <?=$dados["sexoContato"] ?>      </td>
             <td> <?=$dados["dataNascContato"] ?>  </td>
-            <td> <a href="index.php?menuop=editar-contato&idContato=<?=$dados["idContato"] ?>"> Editar </a> </td>
-            <td> <a href="index.php?menuop=excluir-contato&idContato=<?=$dados["idContato"] ?>"> Excluir </a> </td>
+            <td> <a class="btn btn-danger" href="index.php?menuop=editar-contato&idContato=<?=$dados["idContato"] ?>"> Editar </a> </td>
+            <td> <a class="btn btn-success" href="index.php?menuop=excluir-contato&idContato=<?=$dados["idContato"] ?>"> Excluir </a> </td>
         </tr>
     <?php
     }
@@ -73,34 +74,41 @@
     </tbody>
 </table>
 <br>
-<?php
-$sqlTotal = "SELECT idContato FROM tbContatos";
-$qrTotal = mysqli_query($conexao, $sqlTotal) or die(mysqli_error($conexao));
-$numTotal = mysqli_num_rows($qrTotal);
-$totalPagina = ceil($numTotal/$quantidade);
-echo "Total de Contatos: $numTotal <br>";
-echo "<a href=\"?menuop=contatos&pagina=1\">Primeira Página</a>";
-
-if($pagina>6){
-    ?>
-    <a href="?menuop=contatos&pagina=<?php echo $pagina-1?>"> << </a>
+<div data-bs-theme="dark">
+    <ul class="pagination justify-content-center">
     <?php
-}
+    $sqlTotal = "SELECT idContato FROM tbcontatos";
+    $qrTotal = mysqli_query($conexao, $sqlTotal) or die(mysqli_error($conexao));
+    $numTotal = mysqli_num_rows($qrTotal);
+    $totalPagina = ceil($numTotal / $quantidade);
 
-for($i=1; $i<=$totalPagina; $i++){
-    if($i >= ($pagina-5) && $i <= ($pagina + 5)){
-        if($i == $pagina){
-            echo $i;
-        }else{
-            echo "<a href=\"?menuop=contatos&pagina=$i\">$i</a> ";
+    # echo "Total de Contatos: $numTotal";
+
+    echo "<li class=\"page-item\"><a class=\"page-link\" href=\"?menuop=contatos&pagina=1\">Primeira Página</a></li>";
+
+    if ($pagina > 6) {
+        ?>
+        <li class="page-item"><a class="page-link" href="?menuop=contatos&pagina=<?php echo $pagina - 1; ?>"> << </a></li>
+        <?php
+    }
+
+    for ($i = 1; $i <= $totalPagina; $i++) {
+        if ($i >= ($pagina - 5) && $i <= ($pagina + 5)) {
+            if ($i == $pagina) {
+                echo "<li class=\"page-item active\"><span class=\"page-link\">$i</span></li>";
+            } else {
+                echo "<li class=\"page-item\"><a class=\"page-link\" href=\"?menuop=contatos&pagina=$i\">$i</a></li>";
+            }
         }
     }
-}
-if($pagina< ($totalPagina-5)){
-    ?>
-    <a href="?menuop=contatos&pagina=<?php echo $pagina+1?>"> >> </a>
-    <?php
-}
 
-echo "<a href=\"?menuop=contatos&pagina=$totalPagina\">Ultima Página</a>";
-?>
+    if ($pagina < ($totalPagina - 5)) {
+        ?>
+        <li class="page-item"><a class="page-link" href="?menuop=contatos&pagina=<?php echo $pagina + 1; ?>"> >> </a></li>
+        <?php
+    }
+
+    echo "<li class=\"page-item\"><a class=\"page-link\" href=\"?menuop=contatos&pagina=$totalPagina\">Última Página</a></li>";
+    ?>
+    </ul>
+</div>
