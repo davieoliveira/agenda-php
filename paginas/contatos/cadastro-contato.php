8a838e6330ad2c02ea9034e2b7082a66568fd892
davieoliveira <emanuel.davi@escolar.ifrn.edu.br>
2026-08-05 12:33:31 -0300

    Atualizado bootstrap nas telas de cadastro e editacao de contato

--- paginas/contatos/cadastro-contato.php
+++ paginas/contatos/cadastro-contato.php
@@ -3,30 +3,53 @@
 </header>
 <div>
     <form action="index.php?menuop=inserir-contato" method="post">
-        <div>
-            <label for="nomeContato"> Nome</label>
-            <input type="text" name="nomeContato">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6"> 
+                    <!-- Nome de Contato -->
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1"> <i class="bi bi-person"></i> </span>
+                        <input type="text" class="form-control" placeholder="Nome" aria-label="Username" name="nomeContato">
+                    </div>
+                    
+                    <!-- Email do Contato -->
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon2"> <i class="bi bi-envelope"></i> </span>
+                        <input type="email" class="form-control" placeholder="E-mail" aria-label="Email" name="emailContato">
+                    </div>
+
+                     <!-- Telefone do Contato -->
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon2"> <i class="bi bi-telephone-fill"></i> </span>
+                        <input type="text" class="form-control" placeholder="Telefone" aria-label="Email" name="telefoneContato">
+                    </div>
+                    
+                     <!-- Endereço do Contato -->
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon2"> <i class="bi bi-geo-alt-fill"></i> </span>
+                        <input type="text" class="form-control" placeholder="Endereço" aria-label="Email" name="enderecoContato">
+                    </div>
+
+                     <!-- Data de Nascimento do Contato -->
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon2"> <i class="bi bi-calendar2-date"></i> </span>
+                        <input type="date" class="form-control" placeholder="Data de Nascimento" aria-label="Email" name="dataNascContato">
+                    </div>
+                     <!-- Sexo do Contato -->
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon2">
+                            <i class="bi bi-gender-ambiguous"></i>
+                        </span>
+                        <select class="form-select" name="sexoContato" aria-label="Gênero">
+                            <option selected disabled value="">Selecione o gênero</option>
+                            <option value="M">Masculino</option>
+                            <option value="F">Feminino</option>
+                        </select>
+                    </div>  
+                    <div class="d-grid gap-2">
+                        <input class="btn btn-primary" type="submit" value="Adicionar" name="btnAdicionar">
+                    <div>
+            </div>
         </div>
-        <div>
-            <label for="emailContato"> Email </label>
-            <input type="email" name="emailContato">
-        </div>
-        <div>
-            <label for="telefoneContato"> Telefone </label>
-            <input type="text" name="telefoneContato">
-        </div>
-        <div>
-            <label for="enderecoContato"> Endereço </label>
-            <input type="text" name="enderecoContato">
-        </div>
-        <div>
-            <label for="dataNascContato"> Data Nascimento </label>
-            <input type="date" name="dataNascContato">
-        </div>
-        <div>
-            <label for="sexoContato"> Gênero </label>
-            <input type="text" name="sexoContato">
-        </div>
-        <input type="submit" value="Adicionar" name="btnAdicionar">
     </form>
 </div>
--- paginas/contatos/editar-contato.php
+++ paginas/contatos/editar-contato.php
@@ -10,34 +10,61 @@ $dados = mysqli_fetch_assoc($rs);
 </header>
 <div>
     <form action="index.php?menuop=atualizar-contato" method="post">
-        <div>
-            <label for="idContato"> Nome</label>
-            <input type="text" name="idContato" value="<?=$dados["idContato"]?>">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+
+                    <!-- ID do Contato -->
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon0"> <i class="bi bi-key-fill"></i> </span>
+                        <input type="text" class="form-control" placeholder="ID" aria-label="ID" name="idContato" value="<?=$dados["idContato"]?>" readonly>
+                    </div>
+
+                    <!-- Nome do Contato -->
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1"> <i class="bi bi-person"></i> </span>
+                        <input type="text" class="form-control" placeholder="Nome" aria-label="Nome" name="nomeContato" value="<?=$dados["nomeContato"]?>">
+                    </div>
+
+                    <!-- Email do Contato -->
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon2"> <i class="bi bi-envelope"></i> </span>
+                        <input type="email" class="form-control" placeholder="E-mail" aria-label="Email" name="emailContato" value="<?=$dados["emailContato"]?>">
+                    </div>
+
+                    <!-- Telefone do Contato -->
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon3"> <i class="bi bi-telephone-fill"></i> </span>
+                        <input type="text" class="form-control" placeholder="Telefone" aria-label="Telefone" name="telefoneContato" value="<?=$dados["telefoneContato"]?>">
+                    </div>
+
+                    <!-- Endereço do Contato -->
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon4"> <i class="bi bi-geo-alt-fill"></i> </span>
+                        <input type="text" class="form-control" placeholder="Endereço" aria-label="Endereço" name="enderecoContato" value="<?=$dados["enderecoContato"]?>">
+                    </div>
+
+                    <!-- Data de Nascimento do Contato -->
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon5"> <i class="bi bi-calendar2-date"></i> </span>
+                        <input type="date" class="form-control" aria-label="Data de Nascimento" name="dataNascContato" id="dataNascContato" value="<?= !empty($dados['dataNascContato']) ? date('Y-m-d', strtotime(str_replace('/', '-', $dados['dataNascContato']))) : '' ?>">
+                    </div>
+
+                    <!-- Sexo do Contato -->
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon6"> <i class="bi bi-gender-ambiguous"></i> </span>
+                        <select class="form-select" name="sexoContato" aria-label="Gênero">
+                            <option value="" disabled <?= empty($dados['sexoContato']) ? 'selected' : '' ?>>Selecione o gênero</option>
+                            <option value="M" <?= ($dados['sexoContato'] == 'M' || $dados['sexoContato'] == 'Masculino') ? 'selected' : '' ?>>Masculino</option>
+                            <option value="F" <?= ($dados['sexoContato'] == 'F' || $dados['sexoContato'] == 'Feminino') ? 'selected' : '' ?>>Feminino</option>
+                        </select>
+                    </div>
+
+                    <!-- Botão de Atualizar -->
+                    <div class="d-grid gap-2">
+                        <input class="btn btn-warning" type="submit" value="Atualizar" name="btnAtualizar">
+                    </div>
+            </div>
         </div>
-        <div>
-            <label for="nomeContato"> Nome</label>
-            <input type="text" name="nomeContato" value="<?=$dados["nomeContato"]?>">
-        </div>
-        <div>
-            <label for="emailContato"> Email </label>
-            <input type="email" name="emailContato"value="<?=$dados["emailContato"]?>">
-        </div>
-        <div>
-            <label for="telefoneContato"> Telefone </label>
-            <input type="text" name="telefoneContato" value="<?=$dados["telefoneContato"]?>">
-        </div>
-        <div>
-            <label for="enderecoContato"> Endereço </label>
-            <input type="text" name="enderecoContato" value="<?=$dados["enderecoContato"]?>">
-        </div>
-        <div>
-            <label for="dataNascContato">Data Nascimento</label>
-            <input type="date" name="dataNascContato" id="dataNascContato" value="<?= !empty($dados['dataNascContato']) ? date('Y-m-d', strtotime(str_replace('/', '-', $dados['dataNascContato']))) : '' ?>">
-        </div>
-        <div>
-            <label for="sexoContato"> Gênero </label>
-            <input type="text" name="sexoContato" value="<?=$dados["sexoContato"]?>">
-        </div>
-        <input type="submit" value="Atualizar" name="btnAtualizar">
     </form>
 </div>
